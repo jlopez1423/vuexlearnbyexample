601e4e6b5edc848daa0d6257e752031fda35fe07
Jose <jalopez1423@gmail.com>
2018-07-23 20:42:53 -0700

    added - mapMutations and mapActions

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,9 +3,14 @@
         <div class="left">
             <h1>{{title}}</h1>
 
+                <form @submit.prevent="addLink">
+                    <input type="text" class="link-input" placeholder="Add a Link" v-model="newLink">
+                </form>
+
             <ul>
                 <li v-for="(link, index) in links" v-bind:key="index">
                     {{link}}
+                    <button v-on:click="removeLinks(index)" class="rm">Remove</button>
                 </li>
             </ul>
         </div>
@@ -18,10 +23,15 @@
 
 <script>
     import Stats from '@/components/Stats.vue'
-    import {mapState} from 'vuex'
+    import {mapState, mapMutations, mapActions} from 'vuex'
 
     export default {
         name: 'HelloWorld',
+        data() {
+            return {
+                newLink: ''
+            }
+        },
         components: {
             Stats,
         },
@@ -29,8 +39,22 @@
             ...mapState([
                 'title',
                 'links'
+            ])
+        },
+        methods: {
+            ...mapMutations([
+                'ADD_LINK'
             ]),
-            //Now we can have other things here
+            ...mapActions([
+                'removeLink'
+            ]),
+            addLink: function() {
+                this.ADD_LINK(this.newLink);
+                this.newLink = '';
+            },
+            removeLinks: function(link){
+                this.removeLink(link);
+            }
         }
     }
 </script>
@@ -73,5 +97,24 @@
         background-color: #E9E9E9;
     }
 
+    input {
+        border: none;
+        padding: 20px;
+        width: calc(100% - 40px);
+        box-shadow: 0 5px 5px lightgrey;
+        margin-bottom: 50px;
+        outline: none;
+    }
+
+    .rm {
+        float: right;
+        text-transform: uppercase;
+        font-size: .8em;
+        background: #f9d0e3;
+        border: none;
+        padding: 5px;
+        color: #ff0076;
+        cursor: pointer;
+    }
 </style>
 
